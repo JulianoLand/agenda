8eeeaab82231d86b9373f8e084f0a34fdf699084
Julianoland <juliannojp@gmail.com>
2025-02-10 22:28:06 -0300

    login, logout e segurança

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -15,31 +16,58 @@ class Login {
         this.user = null;
     }
 
-    async register(){
+    async login() {
         this.valida();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
 
-        try {
-            this.user = await LoginModel.create(this.body);
-        } catch (error) {
-            console.log(error);
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if(!this.user){
+            this.errors.push('Usuário ou senha inválidos'); // Na verdade o usuario pode não exisir
+            return;
+        }
+
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+            this.errors.push('Senha inválida');
+            this.user = null;
+            return;
         }
     }
 
-    valida(){
+    async register() {
+        this.valida();
+        if (this.errors.length > 0) return;
+
+        await this.userExists();
+
+        if (this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+
+        this.user = await LoginModel.create(this.body);
+    }
+
+    async userExists() {
+        this.user = await LoginModel.findOne({ email: this.body.email });
+        if (this.user) this.errors.push('Usuário já existe.');
+    }
+
+    valida() {
         this.cleanUp();
 
         // email precisa ser valido
-        if(!validator.isEmail(this.body.email)) this.errors.push('e-mail inválido');
+        if (!validator.isEmail(this.body.email)) this.errors.push('e-mail inválido');
         // A senha precisa ter entre 6 e 15 caracteres
-        if(!this.body.password || this.body.password.length < 6 || this.body.password.length > 15) {
+        if (!this.body.password || this.body.password.length < 6 || this.body.password.length > 15) {
             this.errors.push('A senha precisa ter entre 6 e 15 caracteres.');
         }
     }
 
-    cleanUp(){
-        for(const key in this.body){
-            if(typeof this.body[key] !== 'string'){
+    cleanUp() {
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
         }
