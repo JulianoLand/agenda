9e6bd0779912c3f8e47da84a944610a932771b57
Julianoland <juliannojp@gmail.com>
2025-02-11 22:59:26 -0300

    create user, create contato

---
+++ src/models/ContatoModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const ContatoSchema = new mongoose.Schema({
+    nome: { type: String, required: true },
+    sobrenome: { type: String, required: false, default:'' },
+    email: { type: String, required: false, default:'' },
+    telefone: { type: String, required: false, default:'' },
+    criadoEm: { type: Date, default: Date.now }
+})
+
+const ContatoModel = mongoose.model('Contato', ContatoSchema); //No lugar de 'blabla' podemos colocar o nome da coleção
+
+function Contato(body) {
+    this.body = body;
+    this.errors = [];
+    this.contato = null;
+}
+
+Contato.prototype.register = async function() {
+    this.valida();
+
+    if(this.errors.length > 0) return;
+    this.contato = await ContatoModel.create(this.body);
+};
+
+Contato.prototype.valida = function() {
+    this.cleanUp();
+
+    // email precisa ser valido
+    if(this.body.email && !validator.isEmail(this.body.email)) this.errors.push('e-mail inválido');
+    if(!this.body.nome) this.errors.push('Nome é um campo obrigatório');
+    if(!this.body.email && !this.body.telefone) { 
+        this.errors.push('Pelo menos um contato precisa ser enviado: email ou telefone');
+    }
+}
+
+Contato.prototype.cleanUp = function() {
+    for (const key in this.body) {
+        if (typeof this.body[key] !== 'string') {
+            this.body[key] = '';
+        }
+    }
+
+    this.body = {
+        nome: this.body.nome.trim(),
+        sobrenome: this.body.sobrenome.trim(),
+        email: this.body.email.trim(),
+        telefone: this.body.telefone.trim()
+    };
+}
+
+module.exports = Contato;
